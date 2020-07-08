568012cdb033a6e7bcf49928daec51629c472d7b
hieu <hieutran2811@gmail.com>
2020-07-08 15:57:10 +0700

    them user

---
+++ src/main/java/com/huan/demo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.huan.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/user")
+    public String login(){
+        return "test";
+    }
+}
