3ccbf9edd094819d167aa549d81e0021c7980e6f
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-07-08 15:25:52 +0700

    second commit

--- src/main/java/com/huan/demo/controller/WeatherControllers.java
+++ src/main/java/com/huan/demo/controller/WeatherControllers.java
@@ -2,11 +2,17 @@ package com.huan.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WeatherControllers {
     @GetMapping("/hello")
     public String ALL(){
-        return "hellllooo aaaa";
+        return "/hello";
+    }
+    // phần này để test git
+    @GetMapping("")
+    public String test(){
+        return "/test";
     }
 }
